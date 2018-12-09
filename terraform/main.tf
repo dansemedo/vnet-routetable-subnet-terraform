2d42c90f8b8b63eb329a0793689c1d9770a775d9
Daniel Semedo <danpin@microsoft.com>
2018-12-09 00:13:55 -0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ resource "azurerm_subnet" "subnet1" {
   virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
   resource_group_name  = "${azurerm_resource_group.cgrg.name}"
   route_table_id	     = "${azurerm_route_table.rt.id}"
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "10.0.1.0/24"
 }
 
 output "subnet_id_1" {
@@ -66,7 +66,7 @@ resource "azurerm_subnet_route_table_association" "subnetroute1" {
 #Subnet Config 2
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = "backend"
+  name                 = "frontend"
   virtual_network_name = "${azurerm_virtual_network.vnet2.name}"
   resource_group_name  = "${azurerm_resource_group.cgrg.name}"
   route_table_id       = "${azurerm_route_table.rt.id}"
